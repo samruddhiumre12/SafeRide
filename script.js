ad8f37f80cb57430dbf4e880f3837ea8884de07e
Samruddhi Umre <umresamruddhi12@gmail.com>
2025-05-11 01:46:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Example of a simple scroll effect for navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scroll
+  ::contentReference[oaicite:0]{index=0}
+   
+  
